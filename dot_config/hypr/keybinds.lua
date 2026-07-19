5f1c45592e1be70753fe9cd4dded9937f20458ba
chewygumxx <cgumxx@pm.me>
2026-07-19 16:52:15 +1000

    fix(hypr): Keybind: Brightness

--- dot_config/hypr/keybinds.lua
+++ dot_config/hypr/keybinds.lua
@@ -97,8 +97,9 @@ local function_keys = function()
     hl.bind("XF86AudioLowerVolume",  hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"))
     hl.bind("XF86AudioRaiseVolume",  hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"))
 
-    hl.bind("XF86MonBrightnessUp",   hl.dsp.exec_cmd("brightnessctl --min-value=100 --exponent s 10%+"))
-    hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl --min-value=100 --exponent s 10%-"))
+    hl.bind("XF86MonBrightnessUp",   hl.dsp.exec_cmd("brightnessctl --exponent set 10%+"))
+    hl.bind("XF86MonBrightnessDown",
+        hl.dsp.exec_cmd("brightnessctl --min-value=$(( $(brightnessctl max) / 10 )) --exponent set 10%-") )
 end
 
 M.setup = function()
