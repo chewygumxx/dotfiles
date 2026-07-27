2dd25e313db20738dd66fbc49d64316440367d0a
chewygumxx <cgumxx@pm.me>
2026-07-27 11:53:58 +1000

    Lazy Commit: Mon 27 Jul 2026 11:53:58 AEST

--- dot_config/zsh/rc/plugin/zsh-completions.rc.zsh
+++ dot_config/zsh/rc/plugin/zsh-completions.rc.zsh
@@ -6,12 +6,18 @@
 #
 #
 
+#
+# To be called before compinit
+#
+
 plugin="zsh-completions"
 
 if [[ ! -d "$ZSH_HOME_DIRS[PLUGIN]/$plugin" ]]; then
     git clone "https://github.com/zsh-users/$plugin.git" \
         "$ZSH_HOME_DIRS[PLUGIN]/$plugin"
 fi
+
+fpath+=("$ZSH_HOME_DIRS[PLUGIN]/$plugin/src")
 
 source "$ZSH_HOME_DIRS[PLUGIN]/$plugin/$plugin.plugin.zsh" 2>/dev/null
 unset plugin
