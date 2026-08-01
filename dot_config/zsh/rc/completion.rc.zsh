7e3d8c9015df468b460b2b81f908e34626214bf2
chewygumxx <cgumxx@pm.me>
2026-08-02 06:12:38 +1000

    feat(eza): Wrapper function for eza

--- dot_config/zsh/rc/completion.rc.zsh
+++ dot_config/zsh/rc/completion.rc.zsh
@@ -36,58 +36,7 @@ zstyle ':completion:*' group-name ''
 zstyle ':completion:*' list-dirs-first true
 zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
 zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
-
-function __set_zls_colors () {
-    autoload -Uz colors && colors
-
-    typeset -Tg ZLS_COLORS zls_colors ':'
-    zls_colors=(
-        "di=$color[bold];$color[blue]"      # Directory
-        "fi=$color[none]"                   # Ordinary file
-        "ex=$color[bold];$color[green]"     # Executable
-    
-        "so=$color[bold];$color[red]"       # Socket
-        "pi=$color[yellow]"                 # Named pipe (FIFO)
-        "bd=$color[bold];$color[yellow]"    # Block device
-        "cd=$color[bold];$color[yellow]"    # Character device
-    
-        "ln=$color[italic];$color[cyan]"    # Symlink
-        "or=$color[italic];$color[blink];$color[reverse];$color[red]"   # Broken symlink
-    
-        "tc=$color[faint];$color[magenta]"   # Filetype trailing character
-    )
-    
-    # Archive
-    for ext in "7z" "gz" "rar" "tar" "zip"; do
-        zls_colors+=("*.$ext=$color[red]")
-    done
-    
-    # Audio
-    for ext in "flac" "m4a" "mka" "mp2" "mp3" "ogg"; do
-        zls_colors+=("*.$ext=$color[italic];$color[magenta]")
-    done
-    
-    # Document
-    for ext in "pdf" "doc" "docx"; do
-        zls_colors+=("*.$ext=$color[cyan]")
-    done
-    
-    # Image
-    for ext in "avif" "bmp" "gif" "ico" "jfif" "jpg" "jpeg" "png" "svg" "tif" "tiff" "webp" "xcf"; do
-        zls_colors+=("*.$ext=$color[magenta]")
-    done
-    
-    # Source
-    for ext in "css" "js" "lua" "py" "rs" "sql" "styl" "tsx" "zsh"; do
-        zls_colors+=("*.$ext=$color[yellow]")
-    done
-    
-    # Video
-    for ext in "avi" "m4v" "mkv" "mov" "mp4" "mpeg" "mpg" "webm"; do
-        zls_colors+=("*.$ext=$color[bold];$color[magenta]")
-    done
-    zstyle ":completion:*" list-colors $zls_colors
-}; __set_zls_colors; unset -f __set_zls_colors
+zstyle ":completion:*" list-colors $ls_colors
 
 zmodload zsh/complist # Must be loaded before compinit call
 bindkey -M menuselect '^h' vi-backward-char
--- dot_config/zsh/rc/util/eza.rc.zsh
+++ dot_config/zsh/rc/util/eza.rc.zsh
@@ -1,27 +1,83 @@
 #!/bin/false
 # vim: expandtab:shiftwidth=4
 
-#
-#
+# 
+# 
 # ~/.config/zsh/rc/util/eza.rc.zsh
-#
-#
+# 
+# 
 
 [[ -o interactive  ]] || return
-(( $+commands[eza] )) || return
+(( $+commands[eza] )) || return 127
+
+eza_opts=(
+    "--all"
+    "--long"
+    "--git"
+    "--color=always"
+    "--hyperlink"
+    "--group-directories-first"
+)
+
+eza_ignore_glob=(
+    "[0-9a-f][0-9a-f]"  # .git/objects/*
+    ".obsidian"
+    ".zettel-notes"
+    #".git"
+)
 
 setopt aliases
 
-alias l="eza --all --oneline --color=always --hyperlink --group-directories-first"
-alias ls='l'
+alias l="eza_wrap"
+alias ll="l --long"
+alias lll='ll --long --total-size --all'
 
-# Details
-alias la='l --absolute'
-alias ll='l --long --header --smart-group --mounts'
-alias lll='ll --total-size'	
+alias la="l --unignore"
+alias lla="ll --unignore"
+alias llla='lll --unignore'
+
+alias ld='l --only-dirs --show-symlinks' # I never use GNU `ld`
+alias lld='ll --only-dirs'
+alias llld='lll --only-dirs'
 
 # Tree ^-^
 alias lt="l --tree" 
 alias llt='ll --tree'
 alias lllt='lll --tree'
-alias ltd='lt --only-dirs'
+
+alias lat="la --tree" 
+alias llat='lla --tree'
+alias lllat='llla --tree'
+
+alias ldt='ld --tree'
+alias lldt='lld --tree'
+alias llldt='llld --tree --follow-symlinks'
+
+function eza_wrap () {
+    local -a opts=( "${eza_opts[@]}" )
+    local -a args=()
+    local -i ignore=1
+    local -i long=0
+
+    while (( $# )); do
+        case "$1" in
+            -u|--unignore) ignore=0; shift;;
+            -l|--long)     long+=1;  shift;;
+            -*)  opts+=( "$1"     ); shift;;
+            *)   args+=( "${1:a}" ); shift;; # Show absolute filepath for --tree
+        esac
+    done
+
+
+    (( ignore )) && opts+=( "--ignore-glob" "${(j:|:)eza_ignore_glob}" )
+
+    if   (( long == 1)); then;
+        opts+=( "--header" "--mounts" "--no-user")
+    elif (( long >= 2)); then;
+        opts+=( "--header" "--mounts" "--group" "--smart-group" )
+    else
+        opts+=( "--no-permissions" "--no-user" "--no-filesize" "--no-time")
+    fi
+
+    \command eza "${opts[@]}" "${args[@]}"
+}
