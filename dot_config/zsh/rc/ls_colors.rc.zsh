9c79d4e7a0b3fc11d9db652fc701c5bb9d6530f8
chewygumxx <cgumxx@pm.me>
2026-08-02 06:15:46 +1000

    feat(eza): LS_COLORS

--- dot_config/zsh/rc/ls_colors.rc.zsh
+++ dot_config/zsh/rc/ls_colors.rc.zsh
@@ -14,7 +14,7 @@
 autoload -Uz colors && colors
 
 typeset -Tg LS_COLORS ls_colors ':'
-zls_colors=(
+ls_colors=(
     "di=$color[bold];$color[blue]"      # Directory
     "fi=$color[none]"                   # Ordinary file
     "ex=$color[bold];$color[green]"     # Executable
@@ -32,30 +32,30 @@ zls_colors=(
 
 # Archive
 for ext in "7z" "gz" "rar" "tar" "zip" "xz"; do
-    zls_colors+=("*.$ext=$color[red]")
+    ls_colors+=("*.$ext=$color[red]")
 done
 
 # Audio
 for ext in "flac" "m4a" "mka" "mp2" "mp3" "ogg"; do
-    zls_colors+=("*.$ext=$color[italic];$color[magenta]")
+    ls_colors+=("*.$ext=$color[italic];$color[magenta]")
 done
 
 # Document
 for ext in "pdf" "doc" "docx"; do
-    zls_colors+=("*.$ext=$color[cyan]")
+    ls_colors+=("*.$ext=$color[cyan]")
 done
 
 # Image
 for ext in "avif" "bmp" "gif" "ico" "jfif" "jpg" "jpeg" "png" "svg" "tif" "tiff" "webp" "xcf"; do
-    zls_colors+=("*.$ext=$color[magenta]")
+    ls_colors+=("*.$ext=$color[magenta]")
 done
 
 # Source
 for ext in "css" "js" "lua" "py" "rs" "sql" "styl" "tsx" "zsh"; do
-    zls_colors+=("*.$ext=$color[yellow]")
+    ls_colors+=("*.$ext=$color[yellow]")
 done
 
 # Video
 for ext in "avi" "m4v" "mkv" "mov" "mp4" "mpeg" "mpg" "webm"; do
-    zls_colors+=("*.$ext=$color[bold];$color[magenta]")
+    ls_colors+=("*.$ext=$color[bold];$color[magenta]")
 done
