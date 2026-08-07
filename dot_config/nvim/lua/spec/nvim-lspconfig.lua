1ee7523a79eb4dc644d1246155b20ec4987514e8
chewygumxx <cgumxx@pm.me>
2026-08-07 18:49:25 +1000

    Lazy Commit: Fri 07 Aug 2026 18:49:25 AEST

---
+++ dot_config/nvim/lua/spec/nvim-lspconfig.lua
@@ -0,0 +1,15 @@
+#!/bin/false
+-- vim: expandtab:shiftwidth=4
+
+--
+--
+-- ~/.config/nvim/lua/spec/nvim-lspconfig.lua
+--
+--
+
+local M = {
+    'neovim/nvim-lspconfig',
+    enabled = true,
+}
+
+return M
