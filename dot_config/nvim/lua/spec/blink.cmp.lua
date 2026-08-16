da5148022217c6620ba64b3b66fec64c4369d260
chewygumxx <cgumxx@pm.me>
2026-08-17 03:54:20 +1000

    feat(nvim): Completion, snippet and markdown

---
+++ dot_config/nvim/lua/spec/blink.cmp.lua
@@ -0,0 +1,67 @@
+#!/bin/false
+-- vim: expandtab:shiftwidth=4:filetype=lua:
+
+-- 
+-- 
+-- ~chewygumxx/dotfiles.git
+-- ::: :/dot_config/nvim/lua/spec/blink.cmp.lua
+-- 
+-- 
+
+-- 
+-- 
+-- 
+
+local M = {
+    "saghen/blink.cmp",
+    version = "1.*",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    opts_extend = { "sources.default" },
+}
+
+M.opts = {
+    appearance = {
+        nerd_font_variant = "normal"
+    },
+    
+    completion = {
+        documentation = { auto_show = false },
+        list = {
+            selection = { preselect = false }, -- For <CR> (Enter) keymap
+        }
+    },
+    
+    sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+    },
+
+    fuzzy = {
+        implementation = "prefer_rust_with_warning"
+    },
+}
+
+-- See :h blink-cmp-config-keymap for defining your own keymap
+-- https://cmp.saghen.dev/configuration/keymap.html
+M.opts.keymap = {
+    preset = "none",
+    
+    ["<C-e>"]   = { "cancel", "fallback" },
+    ["<C-Esc>"] = { "cancel", "fallback" },
+    ["<CR>"]    = { "accept", "fallback" },
+
+    ["<Down>"]  = { "select_next", "fallback" },
+    ["<C-j>"]   = { "select_next", "fallback" },
+    ["<Up>"]    = { "select_prev", "fallback" },
+    ["<C-k>"]   = { "select_prev", "fallback" },
+    
+    ["<Tab>"]   = { "snippet_forward",  "select_next", "fallback" },
+    ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+
+    ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+    ["<C-p>"]   = { "scroll_documentation_up",   "fallback" },
+    ["<C-n>"]   = { "scroll_documentation_down", "fallback" },
+}
+
+
+
+return M
---
+++ dot_config/nvim/lua/util/header.lua
@@ -0,0 +1,131 @@
+#!/bin/false
+-- vim: expandtab:shiftwidth=4:filetype=lua:
+
+--
+--
+-- ~chewygumxx/dotfiles.git
+-- ::: :/dot_config/nvim/lua/util/header.lua
+--
+--
+
+--
+-- Immutable header text for all text file formats
+--
+
+local M = {}
+
+local util_modeline = require("util.modeline")
+local util_shebang  = require("util.shebang")
+local util_git      = require("util.git")
+
+M.insert = function(file, buf, opt)
+    local file = file or vim.fn.expand("%")
+    local buf  = buf  or 0
+    local opt  = opt  or {}
+    local commentstring = opt.commentstring or vim.bo[buf].commentstring
+
+    if commentstring == "" then
+        return
+    end
+
+    local lines = {}
+    if vim.bo[buf].filetype == "markdown" then
+        commentstring = "# %s"
+        lines[#lines + 1] = "---"
+        lines[#lines + 1] = util_modeline.base({
+            et = true,
+            sw = 2,
+            ft = "markdown",
+            commentstring = commentstring
+        })
+    else
+        lines[#lines + 1] = util_shebang.get(file, buf)
+        lines[#lines + 1] = util_modeline.base({
+            et = true,
+            sw = 4,
+            ft = vim.bo[buf].filetype,
+            commentstring = commentstring
+        })
+    end
+
+    local slug
+    local path = util_git.path(file)
+    if path:find(":", 1, true) == 1 then
+        slug = util_git.slug(file)
+    end
+    if path:find("~/.config", 1, true) == 1 then
+        slug = "chewygumxx/dotfiles"
+        path, _ = path:gsub("~/%.config", ":/dot_config")
+    end
+
+    if path then
+        lines[#lines + 1] = ""
+        lines[#lines + 1] = string.format(commentstring, "")
+        lines[#lines + 1] = string.format(commentstring, "")
+        if slug then
+            lines[#lines + 1] = string.format(commentstring, "~" .. slug .. ".git")
+            lines[#lines + 1] = string.format(commentstring, "::: " .. path)
+        else
+            lines[#lines + 1] = string.format(commentstring, path)
+        end
+        lines[#lines + 1] = string.format(commentstring, "")
+        lines[#lines + 1] = string.format(commentstring, "")
+    end
+ 
+    -- Description
+    lines[#lines + 1] = ""
+    lines[#lines + 1] = string.format(commentstring, "")
+    lines[#lines + 1] = string.format(commentstring, "")
+    lines[#lines + 1] = string.format(commentstring, "")
+
+    -- Markdown Frontmatter
+    if vim.bo[buf].filetype == "markdown" then
+        lines[#lines + 1] = ""
+        lines[#lines + 1] = "ctime: " .. os.date("%Y-%m-%d")
+        lines[#lines + 1] = "title: "
+        lines[#lines + 1] = "tags:  [  ]"
+        lines[#lines + 1] = "---"
+        lines[#lines + 1] = ""
+        lines[#lines + 1] = "# "
+    end
+    
+    lines[#lines + 1] = ""
+
+    vim.api.nvim_buf_set_lines(buf, 0, 0, false, lines)
+end
+
+
+
+M.command = function()
+    M.insert(vim.fn.expand("%"), vim.api.nvim_get_current_buf())
+end
+
+M.autocmd = function()
+    vim.api.nvim_create_autocmd("BufNewFile", {
+        group = vim.api.nvim_create_augroup("cgxx.header_template", { clear = true }),
+        desc  = "Marks buffer as new for pending header insertion.",
+        callback = function(aucmd_tbl)
+            vim.b[aucmd_tbl.buf].cgxx_pending_header = true
+        end,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("cgxx.header_template_apply", { clear = true }),
+        desc  = "Inserts templated header once filetype (and commentstring) is known.",
+        callback = function(aucmd_tbl)
+            if vim.b[aucmd_tbl.buf].cgxx_pending_header then
+                vim.b[aucmd_tbl.buf].cgxx_pending_header = nil
+                M.insert(aucmd_tbl.file, aucmd_tbl.buf)
+            end
+        end,
+    })
+end
+
+M.setup = function()
+    vim.api.nvim_create_user_command("XXInsertHeader", M.command,
+        { desc = "Prepend buffer with a header, templated according to filepath and extension." }
+    )
+    M.autocmd()
+end
+
+return M
